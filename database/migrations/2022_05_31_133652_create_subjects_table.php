d554e593bd0590a28eb19fc5b65cda98d7e5ba1a
JudahPraise <judahpraiseocampo@gmail.com>
2022-05-31 20:41:03 +0700

    course model and migration created

---
+++ database/migrations/2022_05_31_133652_create_subjects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_id');
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->string('code');
+            $table->string('title');
+            $table->integer('units');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+}
