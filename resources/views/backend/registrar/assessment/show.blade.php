530f6a5f5780b84e4d11d03f56acb30136c79b5e
JudahPraise <judahpraiseocampo@gmail.com>
2022-07-17 00:42:03 +0700

    student information component created

--- resources/views/backend/registrar/assessment/show.blade.php
+++ resources/views/backend/registrar/assessment/show.blade.php
@@ -11,37 +11,7 @@
           </div>
           <div class="card-body">
             <div class="row">
-              <div class="card-body">
-                <div class="row flex-wrap">
-                  <div class="col-md-3">
-                    <div class="subheader">Name</div>
-                    <p class="h3 mb-3">Judah Praise De Ocampo</p>
-                  </div>
-                  <div class="col-md-3">
-                    <div class="subheader">Age</div>
-                    <p class="h3 mb-3">23</p>
-                  </div>
-                  <div class="col-md-3">
-                    <div class="subheader">Gender</div>
-                    <p class="h3 mb-3">Male</p>
-                  </div>
-                  <div class="col-md-3">
-                    <div class="subheader">Date of Birth</div>
-                    <p class="h3 mb-3">April 15 1999</p>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="subheader">E-mail</div>
-                    <div class="h3 mb-3">judahpraiseocampo@gmail.com</div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="subeader">Contact Number</div>
-                    <div class="h3 mb-3">09186286277</div>
-                  </div>
-                </div>
-                <hr>
-              </div>
+              <x-student-information/>
             </div>
           </div>
         </div>
